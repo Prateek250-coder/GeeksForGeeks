eb6d369529c260e53e4d140a7dba0b4fa9f3c964
Prateek250-coder <145857863+Prateek250-coder@users.noreply.github.com>
2024-09-05 23:00:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.java
+++ Difficulty: Easy/Missing in Array/missing-in-array.java
@@ -30,14 +30,11 @@ class Solution {
 
     // Note that the size of the array is n-1
     int missingNumber(int n, int arr[]) {
-          int sum=(n*(n+1)/2);
-          int count=0;
-          int p=0;
-          for(int i=0;i<arr.length;i++){
-              
-                count+=arr[i];   
-          }
-             p=sum-count;
-            return p;
+         int sum=(n*(n+1))/2;
+         for(int i=0;i<arr.length;i++){
+             sum-=arr[i];
+         }
+         return sum;
+        
     }
 }
