ede6e3f894cd442f59395618c3b2c425676d3ec4
Prateek Verma <145857863+Prateek250-coder@users.noreply.github.com>
2025-07-09 03:27:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Remove Spaces/remove-spaces.java
+++ Difficulty: Basic/Remove Spaces/remove-spaces.java
@@ -1,38 +1,15 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-            String s;
-            s = br.readLine();
-
-            Solution ob = new Solution();
-
-            System.out.println(ob.modify(s));
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 // User function Template for Java
 class Solution {
 
     String modify(String s) {
-        String s1="";
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)!=' '){
-                s1+=s.charAt(i);
-            }
-        }
-        return s1;
-    }
+        // your code here
+        int n=s.length();
+    StringBuilder sb=new StringBuilder();
+     for(int i=0;i<n;i++){
+         if(s.charAt(i)!=' '){
+             sb.append(s.charAt(i));
+         }
+     }
+     return sb.toString();
+     }
 }
