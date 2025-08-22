4a3e59bf45d30444931c38edbe2ca77da9a9d2b6
Prateek Verma <145857863+Prateek250-coder@users.noreply.github.com>
2025-08-23 03:54:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome String/palindrome-string.java
+++ Difficulty: Easy/Palindrome String/palindrome-string.java
@@ -1,39 +1,11 @@
-//{ Driver Code Starts
-import java.io.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            String s = read.readLine(); // Use lowercase 's'
-
-            Solution ob = new Solution();
-            if (ob.isPalindrome(s)) // Check palindrome
-                System.out.println("true");
-            else
-                System.out.println("false");
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
     boolean isPalindrome(String s) {
-        int n = s.length();
-        StringBuilder s1 = new StringBuilder();
-
-        // Reverse the string
-        for (int i = n - 1; i >= 0; i--) {
-            s1.append(s.charAt(i));
+        // code here
+        int n=s.length();
+        StringBuilder sb=new StringBuilder();
+        for(int i=n-1;i>=0;i--){
+                sb.append(s.charAt(i));
         }
-
-        // Direct return by comparing strings
-        return s1.toString().equals(s);
+        return sb.toString().equals(s);
     }
 }
