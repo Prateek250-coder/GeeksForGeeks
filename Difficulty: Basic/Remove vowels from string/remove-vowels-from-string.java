f97c61d6defd24d7e6e219aa28ca52074785260d
Prateek Verma <145857863+Prateek250-coder@users.noreply.github.com>
2025-08-23 04:05:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Remove vowels from string/remove-vowels-from-string.java
+++ Difficulty: Basic/Remove vowels from string/remove-vowels-from-string.java
@@ -1,41 +1,14 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            String s = read.readLine();
-
-            Solution ob = new Solution();
-            String result = ob.removeVowels(s);
-
-            System.out.println(result);
-        }
-    }
-}
-// } Driver Code Ends
-
-
 // User function Template for Java
 
 class Solution {
     String removeVowels(String s) {
-        Stack<Character>st=new Stack<>();
-        st.push('a');
-        st.push('e');
-        st.push('i');
-        st.push('o');
-        st.push('u');
+        // code here
+        s=s.toLowerCase();
+        int n=s.length();
         StringBuilder sb=new StringBuilder();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(!st.contains(ch)){
-                sb.append(ch);
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)!='a'&& s.charAt(i)!='e'&& s.charAt(i)!='i'&& s.charAt(i)!='o'&&s.charAt(i)!='u'){
+                sb.append(s.charAt(i));
             }
         }
         return sb.toString();
