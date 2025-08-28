43aeb717b63f21827c4148638049e271d26bd705
Prateek Verma <145857863+Prateek250-coder@users.noreply.github.com>
2025-08-28 05:47:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.java
@@ -0,0 +1,16 @@
+class Solution {
+    public char nonRepeatingChar(String s) {
+        // code here
+        LinkedHashMap<Character,Integer>map=new LinkedHashMap<>();
+        for(int i=0;i<s.length();i++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        }
+        for(Map.Entry<Character,Integer>entry:map.entrySet()){
+            if(entry.getValue()==1){
+                return entry.getKey();
+            }
+        }
+        return '$';
+    }
+}
+ 
