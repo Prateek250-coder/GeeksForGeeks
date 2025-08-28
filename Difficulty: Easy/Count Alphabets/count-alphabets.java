8e515ab25bf037a86ed2feb75e10ddf08ea8c420
Prateek Verma <145857863+Prateek250-coder@users.noreply.github.com>
2025-08-28 05:52:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count Alphabets/count-alphabets.java
+++ Difficulty: Easy/Count Alphabets/count-alphabets.java
@@ -1,71 +1,15 @@
-//{ Driver Code Starts
-//Initial Template for Java
+// User function Template for Java
 
-import java.io.*;
-import java.util.*; 
-
-class GFG{
-    public static void main(String args[]) throws IOException { 
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        
-        while(t-- > 0){
-            
-            String S = read.readLine();
-            
-
-            Solution ob = new Solution();
-            
-
-            
-            System.out.println(ob.Count(S));
-        }
-    } 
-} 
-// } Driver Code Ends
-
-
-//User function Template for Java
-
-class Solution{
-    static int Count(String S)
-    {
-        S=S.toLowerCase();
-        StringBuilder sb=new StringBuilder();
-        Stack<Character>st=new Stack<>();
-        
-        st.push('a');
-        st.push('b');
-        st.push('c');
-        st.push('d');
-        st.push('e');
-        st.push('f');
-        st.push('g');
-        st.push('h');
-        st.push('i');
-        st.push('j');
-        st.push('k');
-        st.push('l');
-        st.push('m');
-        st.push('n');
-        st.push('o');
-        st.push('p');
-        st.push('q');
-        st.push('r');
-        st.push('s');
-        st.push('t');
-        st.push('u');
-        st.push('v');
-        st.push('w');
-        st.push('x');
-        st.push('y');
-        st.push('z');
-        for(int i=0;i<S.length();i++){
-            char ch=S.charAt(i);
-            if(st.contains(ch)){
-                sb.append(ch);
+class Solution {
+    static int Count(String S) {
+        // code here
+        int n=S.length();
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(Character.isLetter(S.charAt(i))){
+                c++;
             }
         }
-        return sb.length();
+        return c;
     }
 }
