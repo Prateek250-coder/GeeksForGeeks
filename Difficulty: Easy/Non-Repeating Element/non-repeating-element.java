48589ab38469907c0c8d54963479cf167c076344
Prateek Verma <145857863+Prateek250-coder@users.noreply.github.com>
2025-10-31 03:20:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Non-Repeating Element/non-repeating-element.java
+++ Difficulty: Easy/Non-Repeating Element/non-repeating-element.java
@@ -3,17 +3,15 @@
 class Solution {
     public int firstNonRepeating(int[] arr) {
         // Complete the function
-        int ans=0;
-        HashMap<Integer,Integer>map= new HashMap<>();
-        for(int i:arr){
-            map.put(i,map.getOrDefault(i,0)+1);
-        }
-        for(int i=0;i<arr.length;i++){
-            if(map.get(arr[i])==1){
-                ans=arr[i];
-                break;
-            }
-        }
-        return ans;
+       HashMap<Integer,Integer>map=new HashMap<>();
+       for(int i:arr){
+           map.put(i,map.getOrDefault(i,0)+1);
+       }
+       for(int i:arr){
+           if(map.get(i)==1){
+               return i;
+           }
+       }
+       return 0;
     }
 }
