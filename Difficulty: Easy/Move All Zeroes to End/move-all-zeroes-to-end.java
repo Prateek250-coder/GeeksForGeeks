b456ca5c658886fd1053393010b5b3baad7d0175
Prateek Verma <145857863+Prateek250-coder@users.noreply.github.com>
2025-11-10 08:48:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.java
+++ Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.java
@@ -1,49 +1,22 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-        while (tc-- > 0) {
-            String[] inputLine = br.readLine().split(" ");
-            int[] arr = new int[inputLine.length];
-            for (int i = 0; i < inputLine.length; i++) {
-                arr[i] = Integer.parseInt(inputLine[i]);
-            }
-
-            new Solution().pushZerosToEnd(arr);
-            int n = arr.length;
-            for (int i = 0; i < n; i++) {
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
     void pushZerosToEnd(int[] arr) {
-        int n=arr.length;
-        int ar[]=new int[n];
+        // code here
         int x=0;
+        int n=arr.length;
+        int arr1[]=new int[n];
         for(int i=0;i<n;i++){
             if(arr[i]!=0){
-                int temp=arr[i];
-                arr[i]=arr[x];
-                arr[x]=temp;
-                x++;
+                arr1[x++]=arr[i];
+                
             }
         }
+        while(x<n){
+            arr1[x++]=0;
+            }
+            for(int i=0;i<arr1.length;i++){
+                arr[i]=arr1[i];
+            }
+            
+        
     }
 }
