3c457c55e97ba1e1c3076e67ddb242474c4f97a9
Prateek Verma <145857863+Prateek250-coder@users.noreply.github.com>
2026-03-13 00:45:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Remove vowels from string/remove-vowels-from-string.java
+++ Difficulty: Basic/Remove vowels from string/remove-vowels-from-string.java
@@ -3,14 +3,14 @@
 class Solution {
     String removeVowels(String s) {
         // code here
-        s=s.toLowerCase();
         int n=s.length();
         StringBuilder sb=new StringBuilder();
         for(int i=0;i<n;i++){
-            if(s.charAt(i)!='a'&& s.charAt(i)!='e'&& s.charAt(i)!='i'&& s.charAt(i)!='o'&&s.charAt(i)!='u'){
+            if(s.charAt(i)!='a'&& s.charAt(i)!='e'&& s.charAt(i)!='i'&& s.charAt(i)!='o'&& s.charAt(i)!='u'){
                 sb.append(s.charAt(i));
             }
         }
         return sb.toString();
+        
     }
 }
