036e21d56d5533cae86314a6ab57b9850b6d957d
Prateek Verma <145857863+Prateek250-coder@users.noreply.github.com>
2026-03-13 00:53:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Remove Spaces/remove-spaces.java
+++ Difficulty: Basic/Remove Spaces/remove-spaces.java
@@ -3,7 +3,12 @@ class Solution {
 
     String modify(String s) {
         // your code here
-        s=s.replaceAll("\\s","");
-        return s;
-     }
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!=' '){
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
 }
